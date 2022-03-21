14b24af46f3c599c8a6c930feda3c2a2aa80893c
fare4z <fareez@psp.edu.my>
2022-03-21 09:51:14 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,89 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.lblEnter = New System.Windows.Forms.Label()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.btnClick = New System.Windows.Forms.Button()
+        Me.lblOutput = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblEnter
+        '
+        Me.lblEnter.AutoSize = True
+        Me.lblEnter.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblEnter.Location = New System.Drawing.Point(12, 20)
+        Me.lblEnter.Name = "lblEnter"
+        Me.lblEnter.Size = New System.Drawing.Size(136, 21)
+        Me.lblEnter.TabIndex = 0
+        Me.lblEnter.Text = "Enter your name : "
+        '
+        'txtName
+        '
+        Me.txtName.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.txtName.Location = New System.Drawing.Point(154, 17)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(232, 29)
+        Me.txtName.TabIndex = 1
+        '
+        'btnClick
+        '
+        Me.btnClick.Location = New System.Drawing.Point(154, 52)
+        Me.btnClick.Name = "btnClick"
+        Me.btnClick.Size = New System.Drawing.Size(75, 23)
+        Me.btnClick.TabIndex = 2
+        Me.btnClick.Text = "Click Here"
+        Me.btnClick.UseVisualStyleBackColor = True
+        '
+        'lblOutput
+        '
+        Me.lblOutput.AutoSize = True
+        Me.lblOutput.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblOutput.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.lblOutput.Location = New System.Drawing.Point(12, 111)
+        Me.lblOutput.Name = "lblOutput"
+        Me.lblOutput.Size = New System.Drawing.Size(62, 22)
+        Me.lblOutput.TabIndex = 3
+        Me.lblOutput.Text = "Result : "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(422, 193)
+        Me.Controls.Add(Me.lblOutput)
+        Me.Controls.Add(Me.btnClick)
+        Me.Controls.Add(Me.txtName)
+        Me.Controls.Add(Me.lblEnter)
+        Me.Name = "Form1"
+        Me.Text = "Input box"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblEnter As Label
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents btnClick As Button
+    Friend WithEvents lblOutput As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnClick_Click(sender As Object, e As EventArgs) Handles btnClick.Click
+        Dim strNama As String = txtName.Text
+        Dim strOutput As String = "Hello " + strNama
+
+        lblOutput.Text = strOutput
+
+    End Sub
+End Class
